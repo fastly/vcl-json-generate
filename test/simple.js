1932a9bf071627de8eb8864a86959691f5e1ba58
Leon Brocard <lbrocard@fastly.com>
2017-08-08 10:47:54 +0100

    Use promises instead of callbacks
    Simplify and slightly modernize by using promises instead of callbacks.

--- test/simple.js
+++ test/simple.js
@@ -14,27 +14,25 @@ const paths = [
   "/objects"
 ];
 
-it(`receives an HTML response from /`, (done) => {
+it('receives an HTML response from /', () =>
   chai.request(uri)
     .get('/')
-    .end((err, res) => {
+    .then(res => {
       expect(res).to.have.status(200);
       expect(res).to.be.html;
       expect(res.text).to.include('<pre>');
-      done();
-  });
-});
+    })
+);
 
 paths.forEach((path) => {
-  it(`receives a JSON response from ${path}`, (done) => {
+  it(`receives a JSON response from ${path}`, () =>
     chai.request(uri)
       .get(path)
-      .end((err, res) => {
+      .then(res => {
         expect(res).to.have.status(200);
         expect(res).to.be.json;
         expect(res.body).to.be.an('object');
-        done();
-    });
-  });
+      })
+  );
 });
 
